00fe2501c6e2183f28b2567741ef5df81114d0c3
BlastLong <vertblast@naver.com>
2023-10-23 16:04:55 +0900

    feat: Change to time check function

--- time_check_exmaple.py
+++ time_check_exmaple.py
@@ -1,6 +1,21 @@
 import time
+"""
 start_time = time.time()
 
 end_time = time.time()
 elapsed_time = end_time - start_time
 print(f"Spent time : {elapsed_time}s")
+"""
+
+"""
+%t to time
+"""
+def check_process_time(func, message):
+    start_time = time.time()
+
+    func()
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    message = message.replace("%t", f"{elapsed_time}")
+    print(message)
